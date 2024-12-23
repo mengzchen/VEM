ea0ad3a15652e97eaa92fde33fac0cacb4732d5b
Jiani Zheng (FESCO Adecco Human Resources) <v-zhengjiani@microsoft.com>
2024-12-23 11:37:07 +0800

    update val data

--- env.sh
+++ env.sh
@@ -3,25 +3,25 @@
 export blob_dir="https://cloudaigcrdataeus.blob.core.windows.net/tunnel-data-code-eus/zhengjiani"
 export SAS="?sv=2023-01-03&st=2024-12-16T05%3A48%3A43Z&se=2024-12-23T05%3A48%3A00Z&skoid=d42edb90-9b8e-4f54-aaa5-dc6c37cabd88&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-12-16T05%3A48%3A43Z&ske=2024-12-23T05%3A48%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=IQSgsRlQuAkhWjT68YxVSMNVxSg1RJ1bQO4wInJWhaI%3D"
 
-./azcopy copy "$blob_dir/images/aitw_images/general$SAS" ./ --recursive
+# ./azcopy copy "$blob_dir/images/aitw_images/general$SAS" ./ --recursive
 
 # get data
-#sudo apt-get install azcopy
-#
-#if [ ! -d "checkpoints" ]; then
-#    mkdir checkpoints
-#    cd checkpoints
-#    azcopy copy "$blob_dir/checkpoints/Auto-UI-Base$SAS" ./ --recursive
-#    azcopy copy "$blob_dir/checkpoints/Qwen2-VL-7B-Instruct$SAS" ./ --recursive
-#    azcopy copy "$blob_dir/checkpoints/blip2-opt-2.7b$SAS" ./ --recursive
-#    azcopy copy "$blob_dir/checkpoints/roberta-base$SAS" ./ --recursive
-#    cd ../
-#fi
-#
-#if [ ! -d "images" ]; then
-#    azcopy copy "$blob_dir/images$SAS" ./ --recursive
-#fi
+# wget https://aka.ms/downloadazcopy-v10-linux
+# tar -xvf downloadazcopy-v10-linux
 
+if [ ! -d "checkpoints" ]; then
+   mkdir checkpoints
+   cd checkpoints
+   ./azcopy copy "$blob_dir/checkpoints/Auto-UI-Base$SAS" ./ --recursive
+   ./azcopy copy "$blob_dir/checkpoints/Qwen2-VL-7B-Instruct$SAS" ./ --recursive
+   ./azcopy copy "$blob_dir/checkpoints/blip2-opt-2.7b$SAS" ./ --recursive
+   ./azcopy copy "$blob_dir/checkpoints/roberta-base$SAS" ./ --recursive
+   cd ../
+fi
+
+if [ ! -d "images" ]; then
+   ./azcopy copy "$blob_dir/images$SAS" ./ --recursive
+fi
 
 #conda create -n digirl python==3.10 -y
 #conda activate digirl
